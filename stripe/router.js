2b2303847b76f87ec90e54bb4566133e719b571e
Rob Birch <rbirchtree@gmail.com>
2019-07-09 12:53:14 -0700

    set-up server for stripe

--- stripe/router.js
+++ stripe/router.js
@@ -1,6 +1,6 @@
 'use strict';
 const express = require("express");
-const stripe = require("stripe")();
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 //plug in key for stripe here don't commit
 const bodyParser = require('body-parser').text();
 
